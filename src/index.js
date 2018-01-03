778457fa27bf426795608bd508207b3c89d81397
Lingxiao Wang <xwangl213@gmail.com>
2018-01-03 22:30:36 +1100

    piechart tried

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import registerServiceWorker from './registerServiceWorker'
 import Chart from '../src/components/Chart'
+import SZPieChart from '../src/components/SZPieChart'
 
-ReactDOM.render(<Chart />, document.getElementById('root'))
+ReactDOM.render(<SZPieChart />, document.getElementById('root'))
 registerServiceWorker();
