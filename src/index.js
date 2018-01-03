6d74f5ac1d740d60783f8365d68e666488ce3649
Unicar <unicar9@gmail.com>
2018-01-03 17:49:01 +1100

    Added recharts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import '../semantic/dist/semantic.min.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import registerServiceWorker from './registerServiceWorker'
+import Chart from '../src/components/Chart'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Chart />, document.getElementById('root'))
 registerServiceWorker();
