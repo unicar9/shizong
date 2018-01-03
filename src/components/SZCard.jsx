dddddf1b0c7b4732d3321a5eb745e31e669fc295
Unicar <unicar9@gmail.com>
2018-01-03 21:49:17 +1100

    Added piechart file

--- src/components/SZCard.jsx
+++ src/components/SZCard.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Button, Card, Image, Form, Input } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Button, Card, Image, Form, Input } from 'semantic-ui-react'
 
 const activityOptions = [
     { text: "Reading", value: "reading" },
@@ -37,7 +37,4 @@ class SZCard extends Component {
     }
 }
 
-
-
-
-export default SZCard;  
+export default SZCard
