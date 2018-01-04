f10be79754ac9450c7d04281a3b032c56a2fb338
Unicar <unicar9@gmail.com>
2018-01-04 13:08:41 +1100

    Changed SZCard

--- src/components/SZCard.jsx
+++ src/components/SZCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Card, Image, Form, Input } from 'semantic-ui-react'
+import { Button, Image, Form, Input, Segment } from 'semantic-ui-react'
 
 const activityOptions = [
     { text: "Reading", value: "reading" },
@@ -10,29 +10,45 @@ const activityOptions = [
 class SZCard extends Component {
     render(){
         return(
-            <Card>
-                <Card.Content>
-                    <Card.Header>
-                        What Have You Done
-                    </Card.Header>
-                </Card.Content>
-                <Card.Content>
-                    <Form>
-                        <Form.Select label='Activity' options={activityOptions} placeholder='Activity' />
-                        <Form.Group widths='equal'>
-                           <Input
+            // <Card>
+            //     <Card.Content>
+            //         <Card.Header>
+            //             What Have You Done
+            //         </Card.Header>
+            //     </Card.Content>
+            //     <Card.Content>
+            //         <Form>
+            //             <Form.Select label='Activity' options={activityOptions} placeholder='Activity' />
+            //             <Form.Group widths='equal'>
+            //                <Input
+            //                 label='hr'
+            //                 placeholder='Input Hours'
+            //                />
+            //                <Input
+            //                 label='min'
+            //                 placeholder='Input Minutes'
+            //                />
+            //             </Form.Group>
+            //             <Form.Field control={Button} basic color='green'>OK</Form.Field>
+            //         </Form>
+            //     </Card.Content>
+            // </Card>
+            <Segment inverted={this.props.inverted} color={this.props.color}>
+                <Form>
+                    <Form.Select label='Activity' options={activityOptions} placeholder='Activity' />
+                    <Form.Group widths='equal'>
+                        <Input
                             label='hr'
                             placeholder='Input Hours'
-                           />
-                           <Input
+                        />
+                        <Input
                             label='min'
                             placeholder='Input Minutes'
-                           />
-                        </Form.Group>
-                        <Form.Field control={Button} basic color='green'>OK</Form.Field>
-                    </Form>
-                </Card.Content>
-            </Card>
+                        />
+                    </Form.Group>
+                    <Form.Field control={Button} basic color='green'>OK</Form.Field>
+                </Form>
+            </Segment>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import registerServiceWorker from './registerServiceWorker'
 import Chart from '../src/components/Chart'
+import SZCard from '../src/components/SZCard'
 
-ReactDOM.render(<Chart />, document.getElementById('root'))
+ReactDOM.render(<SZCard />, document.getElementById('root'))
 registerServiceWorker();
