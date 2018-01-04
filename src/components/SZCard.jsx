9d2249704b66ba67bb0f73d30cbe5786f7a99567
Unicar <unicar9@gmail.com>
2018-01-04 15:05:05 +1100

    Changed to controlled SZCard component

--- src/components/SZCard.jsx
+++ src/components/SZCard.jsx
@@ -1,51 +1,56 @@
 import React, { Component } from 'react'
-import { Button, Image, Form, Input, Segment } from 'semantic-ui-react'
+import { Button, Header, Form, Segment } from 'semantic-ui-react'
 
 const activityOptions = [
-    { text: "Reading", value: "reading" },
-    { text: "Workout", value: "workout" },
-    { text: "Study", value: "study" }
+    { key: 1, text: "Reading", value: "reading" },
+    { key: 2, text: "Workout", value: "workout" },
+    { key: 3, text: "Study", value: "study" },
+    { key: 4, text: "Meditation", value: "meditation" },
 ]
 
 class SZCard extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleSelectionChange = this.handleSelectionChange.bind(this)
+
+        this.state = {
+            value: '',
+            hour: '',
+            minute: ''
+        }    
+    }
+
+    handleChange(event, name) {
+        this.setState({
+            [name]: event.target.value
+        })
+    }
+
+    handleSelectionChange(event, { value }) {
+        this.setState({ value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+    
     render(){
+        const { value, hour, minute } = this.state
+
         return(
-            // <Card>
-            //     <Card.Content>
-            //         <Card.Header>
-            //             What Have You Done
-            //         </Card.Header>
-            //     </Card.Content>
-            //     <Card.Content>
-            //         <Form>
-            //             <Form.Select label='Activity' options={activityOptions} placeholder='Activity' />
-            //             <Form.Group widths='equal'>
-            //                <Input
-            //                 label='hr'
-            //                 placeholder='Input Hours'
-            //                />
-            //                <Input
-            //                 label='min'
-            //                 placeholder='Input Minutes'
-            //                />
-            //             </Form.Group>
-            //             <Form.Field control={Button} basic color='green'>OK</Form.Field>
-            //         </Form>
-            //     </Card.Content>
-            // </Card>
-            <Segment inverted={this.props.inverted} color={this.props.color}>
-                <Form>
-                    <Form.Select label='Activity' options={activityOptions} placeholder='Activity' />
+            <Segment inverted color={this.props.color}>
+                <Header textAlign="center">Log Your Activity</Header>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Dropdown selection options={activityOptions} placeholder='Activity' value={value} onChange={this.handleSelectionChange} />
+                    
                     <Form.Group widths='equal'>
-                        <Input
-                            label='hr'
-                            placeholder='Input Hours'
-                        />
-                        <Input
-                            label='min'
-                            placeholder='Input Minutes'
-                        />
+                        <Form.Input placeholder='Hour' value={hour} onChange={(event) => this.handleChange(event, "hour")}/>
+                        <Form.Input placeholder='Minute' value={minute} onChange={(event) => this.handleChange(event, "minute")}/>
                     </Form.Group>
+                    
                     <Form.Field control={Button} basic color='green'>OK</Form.Field>
                 </Form>
             </Segment>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import registerServiceWorker from './registerServiceWorker'
-import Chart from '../src/components/Chart'
 import SZCard from '../src/components/SZCard'
 
 ReactDOM.render(<SZCard />, document.getElementById('root'))
